e5fc0324a0f24b5f2dfd58a926b54ca48ee83377
Silver Aabjõe <silver.aabjoe@liewenthal.ee>
2015-05-25 11:28:38 +0300

    initial commit

---
+++ MemUsage/main.Designer.cs
@@ -0,0 +1,139 @@
+﻿using OxyPlot.WindowsForms;
+
+namespace MemUsage
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.plot = new OxyPlot.WindowsForms.PlotView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // plot
+            // 
+            this.plot.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.plot.Location = new System.Drawing.Point(0, 27);
+            this.plot.Name = "plot";
+            this.plot.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plot.Size = new System.Drawing.Size(745, 547);
+            this.plot.TabIndex = 0;
+            this.plot.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plot.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plot.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            this.plot.Click += new System.EventHandler(this.plot_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(4, 2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(689, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Image = global::MemUsage.Properties.Resources.clear5;
+            this.button3.Location = new System.Drawing.Point(722, 550);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(24, 24);
+            this.button3.TabIndex = 4;
+            this.toolTip1.SetToolTip(this.button3, "Clear");
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Image = global::MemUsage.Properties.Resources.play128;
+            this.button2.Location = new System.Drawing.Point(718, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(24, 24);
+            this.button2.TabIndex = 3;
+            this.toolTip1.SetToolTip(this.button2, "Start/Pause");
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Image = global::MemUsage.Properties.Resources.refresh36;
+            this.button1.Location = new System.Drawing.Point(694, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(24, 24);
+            this.button1.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.button1, "Refresh process list");
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.LoadProcesses);
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 574);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.plot);
+            this.Name = "main";
+            this.Text = "MemUsage";
+            this.Load += new System.EventHandler(this.main_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PlotView plot;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ MemUsage/main.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+using System.Windows.Forms;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+
+namespace MemUsage
+{
+    public partial class main : Form
+    {
+        [DllImport("psapi.dll", SetLastError = true)]
+        static extern bool GetProcessMemoryInfo(IntPtr hProcess, out PROCESS_MEMORY_COUNTERS counters, uint size);
+
+        [StructLayout(LayoutKind.Sequential, Size = 40)]
+        private struct PROCESS_MEMORY_COUNTERS
+        {
+            public uint cb;
+            public uint PageFaultCount;
+            public uint PeakWorkingSetSize;
+            public uint WorkingSetSize;
+            public uint QuotaPeakPagedPoolUsage;
+            public uint QuotaPagedPoolUsage;
+            public uint QuotaPeakNonPagedPoolUsage;
+            public uint QuotaNonPagedPoolUsage;
+            public uint PagefileUsage;
+            public uint PeakPagefileUsage;
+        }
+
+        public class Item<T>
+        {
+            public string Text { get; set; }
+            public T Value { get; set; }
+
+            public override string ToString()
+            {
+                return Text;
+            }
+        }
+
+        PlotModel model;
+        private Dictionary<FieldInfo, LineSeries> series;
+
+        public main()
+        {
+            InitializeComponent();
+
+            model = new PlotModel {
+                PlotType = PlotType.XY,
+                Background = OxyColors.White,
+                LegendBackground = OxyColor.FromAColor(140, OxyColors.WhiteSmoke),
+                LegendBorder = OxyColors.Black
+            };
+
+            model.Axes.Add(new LinearAxis
+            {
+                Key = "xAxis",
+                Position = AxisPosition.Bottom,
+                MajorGridlineStyle = LineStyle.Solid,
+                MinorGridlineStyle = LineStyle.Dash,
+                Title = "Time (s)"
+            });
+
+            model.Axes.Add(new LinearAxis
+            {
+                Key = "yAxis1",
+                Position = AxisPosition.Left,
+                MajorGridlineStyle = LineStyle.Solid,
+                MinorGridlineStyle = LineStyle.Dash,
+                Title = "Memory Usage (MiB)"
+            });
+
+            series = new Dictionary<FieldInfo, LineSeries>();
+            var t = typeof(PROCESS_MEMORY_COUNTERS);
+            foreach (var f in t.GetFields(BindingFlags.Public | BindingFlags.Instance))
+            {
+                if(f.Name == "cb") continue;
+                var line = new LineSeries { Title = f.Name };
+                model.Series.Add(line);
+                series.Add(f, line);
+            }
+
+            plot.Model = model;
+        }
+
+        int n = 0;
+        private bool Paused;
+
+        private unsafe void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBox1.SelectedItem == null || !Paused) return;
+
+                var p = Process.GetProcesses().Where(process => process.Id == (comboBox1.SelectedItem as Item<Process>).Value.Id);
+
+                if (!p.Any()) {
+                    p = Process.GetProcesses().Where(process => process.ProcessName == (comboBox1.SelectedItem as Item<Process>).Value.ProcessName);
+                }
+
+                if (!p.Any()) return;
+
+                var i = new PROCESS_MEMORY_COUNTERS {cb = (uint) sizeof (PROCESS_MEMORY_COUNTERS)};
+                if (!GetProcessMemoryInfo(p.Single().Handle, out i, i.cb)) return;
+
+                foreach (var s in series)
+                {
+                    var v = (uint)s.Key.GetValue(i);
+                    s.Value.Points.Add(new DataPoint(n / 100.0, v / (1024.0 * 1024.0)));
+                }
+                n++;
+
+                plot.InvalidatePlot(true);
+            } catch(Exception ex)
+            {}
+        }
+
+        private void plot_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoadProcesses(object sender, EventArgs e)
+        {
+            var previous = comboBox1.SelectedItem as Item<Process>;
+            var ilist = new List<Item<Process>>();
+
+            object selected = null;
+            comboBox1.Items.Clear();
+
+            var wmiQueryString = "SELECT ProcessId, ExecutablePath, CommandLine FROM Win32_Process";
+            using (var searcher = new ManagementObjectSearcher(wmiQueryString))
+            using (var results = searcher.Get())
+            {
+                var query = from p in Process.GetProcesses()
+                            join mo in results.Cast<ManagementObject>()
+                            on p.Id equals (int)(uint)mo["ProcessId"]
+                            select new
+                            {
+                                Process = p,
+                                Path = (string)mo["ExecutablePath"],
+                                CommandLine = (string)mo["CommandLine"],
+                            };
+                
+                foreach (var p in query)
+                {
+                    ilist.Add(new Item<Process> { Text = string.Format("{0} ({1} {2})", p.Process.ProcessName, p.Path, p.Process.Id), Value = p.Process });
+                    if (previous != null && p.Process.Id == previous.Value.Id)
+                    {
+                        selected = ilist.Last();
+                    }
+                }
+
+                comboBox1.Items.AddRange(ilist.ToArray());
+                
+                if (selected == null && previous != null) {
+                    selected = ilist.Single(p => p.Value.ProcessName == previous.Value.ProcessName);
+                }
+
+                comboBox1.SelectedItem = selected;
+            }
+        }
+
+        private void main_Load(object sender, EventArgs e)
+        {
+            LoadProcesses(sender, e);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Paused = !Paused;
+            button2.Image = Paused ? Properties.Resources.play128 : Properties.Resources.pause41;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            foreach (var s in series)
+                s.Value.Points.Clear();
+
+            n = 0;
+        }
+    }
+}
